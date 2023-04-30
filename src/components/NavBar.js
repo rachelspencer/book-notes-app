e7b6a661a34577b9c13a503976bd4bca350a970e
Rachel Spencer <rachelspencer688@gmail.com>
2023-04-30 16:55:27 -0400

    side link formatting started

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './NavBar.css';
 
 function NavBar() {
     return (
-        <div>
+        <div className="nav_container">
             <nav>
-                <ul>
-                    <li>
+                <ul className="nav_style">
+                    <li id="your-library" className="link_text_style">
                         <NavLink exact="true" to="/" className="active-link">Your Library</NavLink>
                     </li>
-                    <li>
+                    <li id="add-new" className="link_text_style">
                         <NavLink to="/add" className="active-link">Add New Book</NavLink>
                     </li>
                 </ul>
