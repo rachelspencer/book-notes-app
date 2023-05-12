12eee1bc962f60e500a23fbd51338aeb5512a551
Rachel Spencer <rachelspencer688@gmail.com>
2023-05-12 16:05:19 -0400

    image placeholder implemented for empty library case

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,25 +1,23 @@
 import BookShow from "./BookShow";
 import './BookList.css';
-import no_books_placeholder from "../placeholders";
 
 function BookList({ isLibrary, books = [] }) {
     if (!books.length) {
         return (
-            <div className="book_list_container">
-            <img
-              src={no_books_placeholder}
-              alt="No books to display"
-              className="placeholder_image"
-            />
+            <div className="library_placeholder">
+                <img 
+                src={require('../placeholders/no_books_placeholder.jpeg')}
+                alt="No books to display"
+                className="no_books_placeholder"
+                />
+                <div class="placeholder-text">No books in library, get adding!</div>
           </div>
         )
-       
     };
 
     const renderBooks = () => books.map((book, i) => {
         return <BookShow isInLibrary={isLibrary} key={i} book={book}/>
     });
-
     
     return (
         <div className="book_list_container">
