9266ac5ae6de5f4546bbdae19fbedcf2608779c7
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-12 16:28:37 -0500

    refactor backend to use mongodb

--- server/index.js
+++ server/index.js
@@ -1,38 +1,74 @@
+// code to use MongoDB
 require('dotenv').config({ path: '../.env' });
-const express = require('express');
+const express = require("express");
 const cors = require('cors');
-const { sequelize } = require('./util/database');
-// no longer using ... const seedDatabase = require('./util/seedData');
-const { User } = require('./models/User');
-const { Book } = require('./models/Book');
 
-
-const PORT = process.env.PORT;
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-User.hasMany(Book);
-Book.belongsTo(User)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const { register, login } = require('./controllers/auth')
-const { getAllBooks } = require('./controllers/books')
-const { addBook } = require('./controllers/userBooks')
 
-// Auth
-app.post('/register', register)
-app.post('/login', login)
-app.get('/books', getAllBooks)
-app.post('/books', addBook);
 
-sequelize.sync()
-.then(() => {
-    //seeds database after sequelize syncs ds, and before starting the server
-    // seedDatabase();
-    console.log('Database sync successful');
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-})
-.catch(error => console.log(error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Code for when app ran using bit.io db
+// require('dotenv').config({ path: '../.env' });
+// const express = require('express');
+// const cors = require('cors');
+// const { sequelize } = require('./util/database');
+// // no longer using ... const seedDatabase = require('./util/seedData');
+// const { User } = require('./models/User');
+// const { Book } = require('./models/Book');
+
+
+// const PORT = process.env.PORT;
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// User.hasMany(Book);
+// Book.belongsTo(User)
+
+// const { register, login } = require('./controllers/auth')
+// const { getAllBooks } = require('./controllers/books')
+// const { addBook } = require('./controllers/userBooks')
+
+// // Auth
+// app.post('/register', register)
+// app.post('/login', login)
+// app.get('/books', getAllBooks)
+// app.post('/books', addBook);
+
+// sequelize.sync()
+// .then(() => {
+//     //seeds database after sequelize syncs ds, and before starting the server
+//     // seedDatabase();
+//     console.log('Database sync successful');
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on port ${PORT}`);
+//     });
+// })
+// .catch(error => console.log(error));
