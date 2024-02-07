84162d3f54180e5822cf1715578da929d62f38ae
Rachel Spencer <rachelspencer688@gmail.com>
2024-02-06 21:51:50 -0500

    add modal btn styles

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -24,6 +24,7 @@ function BookShow({isInLibrary, book}) {
     const actionBar = (
         <div>
             <button onClick={closeModal}>Go to Library</button>
+            <button onClick={closeModal}>Add More books</button>
         </div>
     );
 
