89b2d4fe193e1520473f9a0079223c16db67e3c7
Rachel Spencer <rachelspencer688@gmail.com>
2024-05-13 13:41:02 -0400

    Remove local Sequelize sync code from index.js file in preparation for Supabase integration

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 require('dotenv').config({ path: '../.env' });
 const express = require("express");
 const cors = require('cors');
-const { sequelize } = require('./util/database');
-const { User } = require('./models/User');
-const { Book } = require('./models/Book');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,20 +8,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-User.hasMany(Book);
-Book.belongsTo(User)
-
-const { register, login } = require('./controllers/auth')
-const { getAllBooks } = require('./controllers/books')
-const { addBook } = require('./controllers/userBooks')
-
 // Auth
 app.post('/register', register)
 app.post('/login', login)
 app.get('/books', getAllBooks)
 app.post('/books', addBook);
 
-sequelize.sync()
+supabase.sync()
 .then(() => {
     //seeds database after sequelize syncs ds, and before starting the server
     // seedDatabase();
