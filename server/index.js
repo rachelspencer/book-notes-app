d6931f669393cc8eded7b01c0c87e3bc6868c13a
Rachel Spencer <rachelspencer688@gmail.com>
2024-05-13 14:15:06 -0400

    more index.js clean up

--- server/index.js
+++ server/index.js
@@ -9,18 +9,13 @@ app.use(express.json());
 app.use(cors());
 
 // Auth
-app.post('/register', register)
-app.post('/login', login)
-app.get('/books', getAllBooks)
-app.post('/books', addBook);
+// app.post('/register', register)
+// app.post('/login', login)
+// app.get('/books', getAllBooks)
+// app.post('/books', addBook);
 
-supabase.sync()
-.then(() => {
-    //seeds database after sequelize syncs ds, and before starting the server
-    // seedDatabase();
-    console.log('Database sync successful');
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-})
-.catch(error => console.log(error));
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}).on('error', (error) => {
+    console.error('Server startup error:', error);
+});
