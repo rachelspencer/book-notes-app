d108ddaebb55fcdfb6470e3365230dc05579af4e
Rachel Spencer <rachelspencer688@gmail.com>
2024-05-13 14:16:53 -0400

    comment out model tables for books and users, as these will be created directly in the supabase dashboard

--- server/models/user.js
+++ server/models/user.js
@@ -1,21 +1,21 @@
-//This file defines the structure of the 'user' table in the db
+// //This file defines the structure of the 'user' table in the db
 
-// Used to  define the data types for columns in the db table
-const { DataTypes } = require('sequelize');
-// Imports the Sequelize instance, used to define the data model and interact with the db
-const { sequelize } = require('../util/database.js');
+// // Used to  define the data types for columns in the db table
+// const { DataTypes } = require('sequelize');
+// // Imports the Sequelize instance, used to define the data model and interact with the db
+// const { sequelize } = require('../util/database.js');
 
-module.exports = {
-    // Defines a new model for a 'User' objext. Takes 2 arguments: name of the model ('user') and an object containing the column definitions
-    User: sequelize.define('user', {
-        // Defines 'user' table with three columns: id, username, hashPass and its contraints 
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true
-        },
-        username: DataTypes.STRING,
-        hashedPass:DataTypes.STRING
-    })
-};
+// module.exports = {
+//     // Defines a new model for a 'User' objext. Takes 2 arguments: name of the model ('user') and an object containing the column definitions
+//     User: sequelize.define('user', {
+//         // Defines 'user' table with three columns: id, username, hashPass and its contraints 
+//         id: {
+//             type: DataTypes.INTEGER,
+//             autoIncrement: true,
+//             allowNull: false,
+//             primaryKey: true
+//         },
+//         username: DataTypes.STRING,
+//         hashedPass:DataTypes.STRING
+//     })
+// };
