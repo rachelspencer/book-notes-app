95fd3535cfdf46ed19a651891b8143e37a48057b
Rachel Spencer <rachelspencer688@gmail.com>
2024-06-29 18:37:42 -0400

    alter ev names

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,6 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
